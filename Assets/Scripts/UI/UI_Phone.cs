129a45f3e1d07ec63dc086046d23733003018100
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-08-16 03:17:07 +0900

    [fix] UI_Phone 폴리싱 및 버튼 연결

--- Assets/Scripts/UI/UI_Phone.cs
+++ Assets/Scripts/UI/UI_Phone.cs
@@ -4,6 +4,13 @@ using UnityEngine.UI;
 public class UI_Phone : UISelector
 {
     #region enum
+    enum Buttons
+    {
+        Btn_Icon00_Pressed,
+        Btn_Icon00_Normal,
+        Btn_Icon02_Pressed,
+        Btn_Icon02_Normal,
+    }
 
     enum Images
     {
@@ -13,11 +20,23 @@ public class UI_Phone : UISelector
 
     Image Img_Phone;
 
+    Button Btn_Icon00_Pressed;
+    Button Btn_Icon00_Normal;
+    Button Btn_Icon02_Pressed;
+    Button Btn_Icon02_Normal;
+
+
     protected override void Awake()
     {
         BindImages(typeof(Images));
+        BindButtons(typeof(Buttons));
 
         Img_Phone = GetImage((int)Images.Img_Phone);
+
+        Btn_Icon00_Pressed = GetButton((int)Buttons.Btn_Icon00_Pressed);
+        Btn_Icon00_Normal = GetButton((int)Buttons.Btn_Icon00_Normal);
+        Btn_Icon02_Pressed = GetButton((int)Buttons.Btn_Icon02_Pressed);
+        Btn_Icon02_Normal = GetButton((int)Buttons.Btn_Icon02_Normal);
 
         Img_Phone.sprite = Resources.Load<Sprite>("UI_Phone/Img_Phone");
     }
