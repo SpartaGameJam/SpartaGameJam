98375411af72012a10103c340f794e86e1aa6973
Dev-PKH <tapasse12@gmail.com>
2025-08-16 05:02:51 +0900

    feat: 패턴 구현 완료
    - 랜덤 패턴 기능 구현

--- Assets/Scripts/Pattern/LockPattern.cs
+++ Assets/Scripts/Pattern/LockPattern.cs
@@ -21,6 +21,31 @@ public class LockPattern : MonoBehaviour
 
     private new bool enabled = true;
 
+    public int gridSize = 3;
+
+    private void IdToRC(int id, out int r, out int c)
+    {
+        int z = id;
+        r = z / gridSize; // 행
+        c = z % gridSize; // 열
+    }
+
+    int GetMiddleId(int a, int b)
+    {
+        IdToRC(a, out int ar, out int ac);
+        IdToRC(b, out int br, out int bc);
+
+        int dr = Mathf.Abs(br - ar), dc = Mathf.Abs(bc - ac);
+
+        if (ar == br && dc == 2) return (a + b) / 2; // 같은 행, 열이 2 차이 (0,2)
+        if (ac == bc && dr == 2) return (a + b) / 2; // 같은 열, 행이 2 차이 (0,6)
+        if (dc == dr && dc + dr == 4) // 대각선 일 경우 (0,8)
+        {
+            return (a + b) / 2;
+        }
+        return -1;
+    }
+
     private void Start()
     {
         pointers = new Dictionary<int, PatternPointer>();
@@ -134,9 +159,29 @@ public class LockPattern : MonoBehaviour
             return;
         }
 
+        foreach (var line in lines)
+        {
+            if (line.id == pp.id) // 이미 해당 포인터에 라인이 생성됨
+            {
+                return;
+            }
+        }
 
         if (unlocking) // 버튼을 이미 하나라도 해제했다면 다음 버튼 연결
         {
+            int mid = GetMiddleId(pointerOnEdit.id, pp.id); // 현재 저장된 id와 지금 들어온 점의 id
+
+            if (mid != -1) // 중간에 점이 있다면
+            {
+                lineOnEditRect.sizeDelta = new Vector2(lineOnEditRect.sizeDelta.x, Vector3.
+                Distance(pointers[mid].transform.localPosition, pointerOnEdit.transform.localPosition));
+                lineOnEditRect.rotation = Quaternion.FromToRotation(
+                    Vector3.up, (pointers[mid].transform.localPosition - pointerOnEdit.transform.localPosition)
+                    .normalized);
+
+                TrySetLineEdit(pointers[mid]);
+            }
+
             lineOnEditRect.sizeDelta = new Vector2(lineOnEditRect.sizeDelta.x, Vector3.
                 Distance(pp.transform.localPosition, pointerOnEdit.transform.localPosition));
             lineOnEditRect.rotation = Quaternion.FromToRotation(
