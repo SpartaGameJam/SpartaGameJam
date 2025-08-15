dbb472260fe1ec5326feec31ff43b1ad1f834845
Dev-PKH <tapasse12@gmail.com>
2025-08-15 23:54:45 +0900

    feat: 패턴 기능 구현
    - 패턴기능 구현 완료

---
+++ Assets/Scripts/Pattern/LockPattern.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockPattern : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public Canvas canvas;
+    public Vector3 offsetPos = new Vector3(0, -450f, 0); // PatternContent의 Rect Transform 값
+
+    private Dictionary<int, PatternPointer> pointers;
+
+    public List<PatternPointer> lines; // 라인이 어떤 Circle로 부터 생성된지를 파악
+
+    private GameObject lineOnEdit; // 마우스의 포인터를 따라갈 Line
+    private RectTransform lineOnEditRect; // 마우스 위치를 저장
+    private PatternPointer pointerOnEdit; // 포인터를 저장
+
+    private bool unlocking;
+
+    private new bool enabled = true;
+
+    private void Start()
+    {
+        pointers = new Dictionary<int, PatternPointer>();
+
+        for(int i=0; i<transform.childCount; i++)
+        {
+            var point = transform.GetChild(i);
+            var identifier = point.GetComponent<PatternPointer>();
+
+            identifier.id = i;
+
+            pointers.Add(i, identifier);
+        }
+    }
+
+    private void Update()
+    {
+        if(enabled == false)
+        {
+            return;
+        }
+
+        if(unlocking)
+        {
+            Vector3 mousePos = canvas.transform.InverseTransformPoint(Input.mousePosition);
+            mousePos -= offsetPos; // 마우스 위치 동기화
+
+            lineOnEditRect.sizeDelta = new Vector2(lineOnEditRect.sizeDelta.x, 
+                Vector3.Distance(mousePos, pointerOnEdit.transform.localPosition));
+
+            lineOnEditRect.rotation = Quaternion.FromToRotation(
+                Vector3.up, (mousePos - pointerOnEdit.transform.localPosition).normalized);
+        }
+    }
+
+    private IEnumerator Release() // 패턴 제거
+    {
+        enabled = false;
+
+        yield return new WaitForSeconds(3);
+
+        foreach(var point in pointers)
+        {
+            point.Value.GetComponent<Image>().color = Color.white;
+            point.Value.GetComponent<Animator>().enabled = false;
+        }
+
+        foreach (var line in lines)
+        {
+            Destroy(line.gameObject);
+        }
+
+        lines.Clear();
+
+        lineOnEdit = null;
+        lineOnEditRect = null;
+        pointerOnEdit = null;
+
+        enabled = true;
+    }
+
+    /// <summary>
+    /// 라인을 포인터의 위치에 맞게 생성
+    /// </summary>
+    /// <param name="pos">포인터 위치</param>
+    /// <param name="id">포인터 id</param>
+    private GameObject CreateLine(Vector3 pos, int id)
+    {
+        var line = GameObject.Instantiate(linePrefab, canvas.transform);
+
+        line.transform.localPosition = pos;
+
+        var linePointer = line.AddComponent<PatternPointer>();
+
+        linePointer.id = id;
+
+        lines.Add(linePointer);
+
+        return line;
+    }
+
+    /// <summary>
+    /// 포인터를 고려하여 라인을 생성
+    /// </summary>
+    /// <param name="pp"></param>
+    private void TrySetLineEdit(PatternPointer pp) 
+    {
+        foreach(var line in lines)
+        {
+            if(line.id == pp.id) // 이미 해당 포인터에 라인이 생성됨
+            {
+                return;
+            }
+        }
+
+        lineOnEdit = CreateLine(pp.transform.localPosition + offsetPos , pp.id); // 첫 생성 위치 동기화
+        lineOnEditRect = lineOnEdit.GetComponent<RectTransform>();
+        pointerOnEdit = pp;
+    }
+
+    private void EnableColorFade(Animator anim)
+    {
+        anim.enabled = true;
+        anim.Rebind();
+    }
+
+    public void MouseEnter(PatternPointer pp)
+    {
+        if (enabled == false)
+        {
+            return;
+        }
+
+
+        if (unlocking) // 버튼을 이미 하나라도 해제했다면 다음 버튼 연결
+        {
+            lineOnEditRect.sizeDelta = new Vector2(lineOnEditRect.sizeDelta.x, Vector3.
+                Distance(pp.transform.localPosition, pointerOnEdit.transform.localPosition));
+            lineOnEditRect.rotation = Quaternion.FromToRotation(
+                Vector3.up, (pp.transform.localPosition - pointerOnEdit.transform.localPosition)
+                .normalized);
+
+            TrySetLineEdit(pp);
+        }
+    }
+
+    public void MouseExit(PatternPointer pp)
+    {
+
+        if (enabled == false)
+        {
+            return;
+        }
+
+        //Debug.Log("Exit : " + pp.id);
+    }
+
+    public void MouseDown(PatternPointer pp)
+    {
+        //Debug.Log("Down : " + pp.id);
+
+        if (enabled == false)
+        {
+            return;
+        }
+
+
+        unlocking = true;
+
+        TrySetLineEdit(pp);
+    }
+
+    public void MouseUp(PatternPointer pp)
+    {
+        //Debug.Log("Up : " + pp.id);
+
+        if (enabled == false)
+        {
+            return;
+        }
+
+
+        if (unlocking)
+        {
+            foreach(var line in lines)
+            {
+                EnableColorFade(pointers[line.id].gameObject.GetComponent<Animator>());
+            }
+
+            Destroy(lines[lines.Count - 1].gameObject);
+            lines.RemoveAt(lines.Count - 1);
+
+            foreach(var line in lines)
+            {
+                EnableColorFade(line.GetComponent<Animator>());
+            }
+
+            StartCoroutine(Release());
+        }
+
+        unlocking = false;
+
+        
+
+    }
+}
---
+++ Assets/Scripts/Pattern/PatternPointer.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PatternPointer : MonoBehaviour
+{
+    public int id;
+
+}
