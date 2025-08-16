24302283809c2f02b5d5143a89d00e3417f30a44
Dev-PKH <tapasse12@gmail.com>
2025-08-16 10:00:38 +0900

    feat: 패턴 기능 구현 완성
    - 중복 모양 패턴 로직 처리

--- Assets/Scripts/Pattern/MonitorPattern.cs
+++ Assets/Scripts/Pattern/MonitorPattern.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MonitorPattern : MonoBehaviour
@@ -16,12 +17,115 @@ public class MonitorPattern : MonoBehaviour
     //public Vector3 offsetPos = new Vector3(0, 450f, 0); // Monitor의 Rect Transform 값
     public float yOffset = 350f;
 
+    // 세 점이 같은 행/열/대각선(↘ 또는 ↙) 위에 있는지
+    private bool IsStraightLine(int a, int b, int c)
+    {
+        IdToRC(a, out int ar, out int ac);
+        IdToRC(b, out int br, out int bc);
+        IdToRC(c, out int cr, out int cc);
+
+        bool sameRow = (ar == br) && (br == cr);
+        bool sameCol = (ac == bc) && (bc == cc);
+
+        bool mainDiag = (ar - ac) == (br - bc) && (br - bc) == (cr - cc); // ↘ 대각(r-c 일정)
+        bool antiDiag = (ar + ac) == (br + bc) && (br + bc) == (cr + cc); // ↙ 대각(r+c 일정)
+
+        return sameRow || sameCol || mainDiag || antiDiag;
+    }
+
+    // 정답 열쇠
+    private HashSet<string> answerSet = new HashSet<string>();
+
+    private void RebuildAnswerSet()
+    {
+        int first = -1; // 첫 연속 행렬
+        int last = -1; // 마지막 연속 행렬
+
+        for(int i=0; i< patternSequence.Count-2; i++)
+        {
+            if (IsStraightLine(patternSequence[i], patternSequence[i+1], patternSequence[i+2]))
+            {
+                if (first == -1) first = i; // 첫번째 위치 선택 ex) 0 1 2 라면 0을 선택
+                else last = i+1; // 마지막에서 2번째 위치 ex) 6 7 8 이면 7의 인덱스가 선택
+            }
+        }
+
+        string tmp1 = ""; // 그대로
+        string tmp2 = ""; // 앞만 뒤집
+        string tmp3 = ""; // 뒤만 뒤집
+        string tmp4 = ""; // 둘다 뒤집
+        string reverse = ""; // 아예 역순
+
+        for(int i=0; i<patternSequence.Count; i++)
+        {
+            tmp1 += patternSequence[i].ToString();
+
+            if (first == i) // 앞에 뒤집을 순간
+            {
+                tmp1 += patternSequence[i + 1].ToString(); // 건너뜀 방지
+                tmp2 += patternSequence[i+1].ToString() + patternSequence[i].ToString();
+                tmp3 += patternSequence[i].ToString() + patternSequence[i + 1].ToString();
+                tmp4 += patternSequence[i+1].ToString() + patternSequence[i].ToString();
+                i++;
+                continue;
+            }
+            else if (last == i) // 뒤에 뒤집을 순간
+            {
+                tmp1 += patternSequence[i + 1].ToString();
+                tmp2 += patternSequence[i].ToString() + patternSequence[i + 1].ToString();
+                tmp3 += patternSequence[i + 1].ToString() + patternSequence[i].ToString();
+                tmp4 += patternSequence[i + 1].ToString() + patternSequence[i].ToString();
+
+                i++;
+                continue;
+            }
+            else // 아무것도 아닐 때
+            {
+                tmp2 += patternSequence[i].ToString();
+                tmp3 += patternSequence[i].ToString();
+                tmp4 += patternSequence[i].ToString();
+            }
+        }
+
+        for (int i = tmp1.Length - 1; i >= 0; i--)
+            reverse += tmp1[i];
+
+        answerSet.Add(reverse);
+
+        answerSet.Add(tmp1);
+        if(first != -1) answerSet.Add(tmp2);
+        if(last != -1) answerSet.Add(tmp3);
+        if(first != -1 && last != -1) answerSet.Add(tmp4);
+
+        Debug.Log(tmp1);
+        Debug.Log(tmp2);
+        Debug.Log(tmp3);
+        Debug.Log(tmp4);
+        Debug.Log(reverse);
+    }
+
     public bool CheckPattern(List<int> pointlist)
     {
-        var setA = new HashSet<int>(patternSequence);
+        /*var setA = new HashSet<int>(patternSequence);
         var setB = new HashSet<int>(pointlist);
 
-        return setA.SetEquals(setB);
+        return setA.SetEquals(setB);*/
+        answerSet.Clear();
+
+        RebuildAnswerSet();
+
+        string result = "";
+        foreach (var point in pointlist) {
+            result += point.ToString();
+        }
+
+        Debug.Log(result);
+        //Debug.Log(answerSet.Contains(result));
+        //Debug.Log(answerSet.Contains("23458"));
+
+        return answerSet.Contains(result);
+
+        //return answerSet.Contains(ToKey(pointlist));
     }
 
     private void Start()
@@ -43,7 +147,15 @@ public class MonitorPattern : MonoBehaviour
     private IEnumerator test()
     {
         yield return null;
-        SetPatternSquence();
+        //SetPatternSquence();
+
+        patternSequence.Add(3);
+        patternSequence.Add(4);
+        patternSequence.Add(5);
+        patternSequence.Add(2);
+        patternSequence.Add(8);
+
+        SetLine();
     }
 
     public void SetPatternSquence()
