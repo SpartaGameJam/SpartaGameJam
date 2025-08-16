42ec382db03059669754c93b59f70671f43b29fe
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-08-16 16:42:09 +0900

    [feat] NPC 터치 시 랜덤 대사 및 표정 출력 기능 추가

--- Assets/Scripts/UI/UI_Shop.cs
+++ Assets/Scripts/UI/UI_Shop.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -26,6 +28,11 @@ public class UI_Shop : UI_Scene
         Img_Cashier
     }
 
+    enum Objects
+    {
+        Obj_Cashier,
+    }
+
     #endregion
 
     Button Btn_ReturnToOffice;
@@ -38,12 +45,15 @@ public class UI_Shop : UI_Scene
     TextMeshProUGUI Txt_CashierDialogue;
     Image Img_Cashier;
 
+    GameObject Obj_Cashier;
+
 
     protected override void Awake()
     {
         BindTexts(typeof(Texts));
         BindButtons(typeof(Buttons));
         BindImages(typeof(Images));
+        BindObjects(typeof(Objects));
 
         Txt_CashierDialogue = GetText((int)Texts.Txt_CashierDialogue);
 
@@ -56,13 +66,77 @@ public class UI_Shop : UI_Scene
 
         Img_Cashier = GetImage((int)Images.Img_Cashier);
 
+        Obj_Cashier = GetObject((int)Objects.Obj_Cashier);
+
         Img_BG02.sprite = Resources.Load<Sprite>("UI_Shop/Img_BG02");
         Img_Counter.sprite = Resources.Load<Sprite>("UI_Shop/Img_Counter");
         Img_ReturnToOffice.sprite = Resources.Load<Sprite>("UI_Shop/Img_ReturnToOffice");
+        Img_Cashier.sprite = Resources.Load<Sprite>("Char/Img_Juno002");
 
         BindEvent(Btn_Lotto.gameObject, OnClickLotto);
         BindEvent(Btn_ReturnToOffice.gameObject, OnReturnToOffice);
+
+        BindEvent(Obj_Cashier, CashierClick);
+
+        _workOriginPos = Obj_Cashier.GetComponent<RectTransform>().anchoredPosition;
     }
+
+    
+    public string GetRandomDialogue()
+    {
+        int index = Random.Range(0, StringNameSpace.dialogues.Length);
+        return StringNameSpace.dialogues[index];
+    }
+
+    public Sprite LoadEmotion(int emotionId)
+    {
+        string emotionName = "Img_Juno" + emotionId.ToString("000");
+        Sprite emotion = Resources.Load<Sprite>($"Char/{emotionName}");
+
+        return emotion;
+    }
+
+    void CashierClick(PointerEventData pointerEventData)
+    {
+        OnClickWorkInstructionPanel();
+
+        int randomIndex = UnityEngine.Random.Range(0, 4);
+        Img_Cashier.sprite = LoadEmotion(randomIndex);
+
+        Txt_CashierDialogue.text = GetRandomDialogue().ToString();
+    }
+
+
+    [SerializeField] private float workTargetPosY = 180f;
+    [SerializeField] private float workMoveDuration = 0.6f;
+
+    private Vector2 _workOriginPos;
+    private bool _workIsAtTarget = false;
+
+
+    public void OnClickWorkInstructionPanel()
+    {
+        RectTransform rt = Obj_Cashier.GetComponent<RectTransform>();
+
+        // DG_MoveEase 없으면 자동 부착
+        DG_MoveEase mover = Obj_Cashier.GetComponent<DG_MoveEase>();
+        if (mover == null) mover = Obj_Cashier.AddComponent<DG_MoveEase>();
+
+        // 현재 위치 기준 토글 (X는 유지, Y만 변경)
+        Vector2 nextPosAnchored = _workIsAtTarget ? _workOriginPos : new Vector2(rt.anchoredPosition.x, 180);
+
+        mover.target = rt;
+        mover.duration = workMoveDuration;
+
+        mover.targetPos = nextPosAnchored;
+
+        // anchoredPosition 기준 이동을 원하므로 Local 버전 호출
+        mover.PlayLocalMove();
+
+        _workIsAtTarget = !_workIsAtTarget;
+    }
+
+
 
     #region Button
     void OnClickLotto(PointerEventData eventData)
--- Assets/Scripts/Utils/StringNameSpace.cs
+++ Assets/Scripts/Utils/StringNameSpace.cs
@@ -7,4 +7,23 @@ public class StringNameSpace : MonoBehaviour
     public const string FeverTriggerRate = "FeverTriggerRate";
     public const string LotteryWinRate = "LotteryWinRate";
     public const string LotteryDiscountRate = "LotteryDiscountRate";
+
+    public const string NPCDialogue00 = "Scratch your ticket before you head out!";
+    public const string NPCDialogue01 = "Give your ticket a scratch before leaving!";
+    public const string NPCDialogue02 = "Don't forget to scratch your ticket before you go!";
+    public const string NPCDialogue03 = "welcome...";
+
+    public static readonly string[] dialogues =
+    {
+        StringNameSpace.NPCDialogue00,
+        StringNameSpace.NPCDialogue01,
+        StringNameSpace.NPCDialogue02,
+        StringNameSpace.NPCDialogue03
+    };
+    
+    public string GetRandomDialogue()
+    {
+        int index = Random.Range(0, dialogues.Length);
+        return dialogues[index];
+    }
 }
