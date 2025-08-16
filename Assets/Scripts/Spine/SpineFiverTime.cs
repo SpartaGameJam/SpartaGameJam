c02bc9847b8496a3be76a651b5f4a117bbec404f
Dev-PKH <tapasse12@gmail.com>
2025-08-16 21:19:13 +0900

    feat: 스파인 컨트롤러 생성
    - 스파인 컨트롤러 생성

---
+++ Assets/Scripts/Spine/SpineFiverTime.cs
@@ -0,0 +1,17 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+public class SpineFiverTime : MonoBehaviour
+{
+    private SkeletonGraphic skeletonGraphic;
+
+    private void Awake()
+    {
+        skeletonGraphic = GetComponent<SkeletonGraphic>();
+    }
+
+    private void Start()
+    {
+        SpineController.Instance.skeletonGraphicTimeFiver = skeletonGraphic;
+    }
+}
